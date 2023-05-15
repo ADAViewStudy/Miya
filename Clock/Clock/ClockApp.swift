b1b1a9f6bb38d1c690277a3b15e12dfa27425829
금가경 <gageum0@gmail.com>
2023-05-16 00:24:03 +0900

    [Setting] Make project file

---
+++ Clock/Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by 금가경 on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
